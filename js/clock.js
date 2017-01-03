32a4f6b8c4629636be4ee0b59335ef5df79dd3fb
Esteban Padilla <ep@estebanpadilla.com>
2017-01-03 12:45:34 -0600

    Save.

--- js/clock.js
+++ js/clock.js
@@ -2,13 +2,25 @@ window.addEventListener('load', init, false);
 
 function init() {
 
+    var canvas = undefined;
+    var ctx = undefined;
+    var secondsArrow = undefined;
+    var minutesArrow = undefined;
+    var hoursArrow = undefined;
+    var clockBackground = undefined;
+    var clockCenter = undefined;
+    var point3 = undefined;
+    var point6 = undefined;
+    var point9 = undefined;
+    var point12 = undefined;
+
     var stats = new Stats();
     document.body.appendChild(stats.dom);
 
     const xpos = 0;
     const ypos = 0;
-    const canvasWidth = 300;
-    const canvasHeight = 300;
+    const width = 300;
+    const height = 300;
 
     const clockXpos = 140;
     const clockYpos = 160;
@@ -17,86 +29,31 @@ function init() {
     var minutes = 0;
     var hours = 0;
 
-    var canvas = document.createElement('canvas');
-    canvas.width = canvasWidth;
-    canvas.height = canvasHeight;
-    canvas.style.background = "#ee3344";
-    canvas.style.position = 'absolute';
-    canvas.style.left = "" + xpos + "px";
-    canvas.style.top = "" + ypos + "px";
-    document.body.appendChild(canvas);
+    canvas = createCanvas(xpos, ypos, width, height);
+    ctx = canvas.getContext('2d');
 
-    var ctx = canvas.getContext('2d');
-
-    function Circle() {
-        this.radius = 100;
-        this.x = 0;
-        this.y = 0;
-        this.lineWidth = 3;
-        this.color = 'none';
-        this.strokeStyle = 'none';
-    }
-
-    Circle.prototype.draw = function draw(context) {
-        context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-        context.fillStyle = this.color;
-        context.fill();
-        context.lineWidth = this.lineWidth;
-        context.strokeStyle = this.strokeStyle;
-
-        context.stroke();
-    };
-
-    function Arrow() {
-        this.length = 0;
-        this.x = 0;
-        this.y = 0;
-        this.rotation = 0;
-        this.lineWidth = 0;
-        this.color = 'white';
-    }
-
-    Arrow.prototype.draw = function (context) {
-        context.save();
-        context.translate(this.x, this.y);
-        context.rotate(this.rotation);
-        context.beginPath();
-        context.moveTo(0, 0);
-        context.lineTo(0, -this.length);
-        context.lineWidth = this.lineWidth;
-        context.strokeStyle = this.color;
-        context.lineCap = 'round';
-        context.shadowBlur = 6;
-        context.shadowOffsetX = 1;
-        context.shadowOffsetY = 4;
-        context.shadowColor = "rgba(100, 100, 100, 0.75)";
-        context.stroke();
-        context.restore();
-    };
-
-    var secondsArrow = new Arrow();
+    secondsArrow = new Arrow();
     secondsArrow.length = 50;
     secondsArrow.lineWidth = 3;
     secondsArrow.x = clockXpos;
     secondsArrow.y = clockYpos;
     secondsArrow.color = '#2b0d3b';
 
-    var minutesArrow = new Arrow();
+    minutesArrow = new Arrow();
     minutesArrow.length = 50;
     minutesArrow.lineWidth = 5;
     minutesArrow.x = clockXpos;
     minutesArrow.y = clockYpos;
     minutesArrow.color = '#2b0d3b';
 
-    var hoursArrow = new Arrow();
+    hoursArrow = new Arrow();
     hoursArrow.length = 50;
     hoursArrow.lineWidth = 10;
     hoursArrow.x = clockXpos;
     hoursArrow.y = clockYpos;
     hoursArrow.color = '#2b0d3b';
 
-    var clockBackground = new Circle();
+    clockBackground = new Circle();
     clockBackground.radius = 75;
     clockBackground.lineWidth = 5;
     clockBackground.x = clockXpos;
@@ -104,7 +61,7 @@ function init() {
     clockBackground.color = '#ffdd17';
     clockBackground.strokeStyle = '#2b0d3b';
 
-    var clockCenter = new Circle();
+    clockCenter = new Circle();
     clockCenter.radius = 10;
     clockCenter.lineWidth = 5;
     clockCenter.x = clockXpos;
@@ -112,7 +69,7 @@ function init() {
     clockCenter.color = '#2b0d3b';
     clockCenter.strokeStyle = '#2b0d3b';
 
-    var point12 = new Circle();
+    point12 = new Circle();
     point12.radius = 1;
     point12.lineWidth = 5;
     point12.x = clockXpos;
@@ -120,7 +77,7 @@ function init() {
     point12.color = '#2b0d3b';
     point12.strokeStyle = '#2b0d3b';
 
-    var point3 = new Circle();
+    point3 = new Circle();
     point3.radius = 1;
     point3.lineWidth = 5;
     point3.x = clockXpos + 61;
@@ -128,7 +85,7 @@ function init() {
     point3.color = '#2b0d3b';
     point3.strokeStyle = '#2b0d3b';
 
-    var point6 = new Circle();
+    point6 = new Circle();
     point6.radius = 1;
     point6.lineWidth = 5;
     point6.x = clockXpos;
@@ -136,14 +93,13 @@ function init() {
     point6.color = '#2b0d3b';
     point6.strokeStyle = '#2b0d3b';
 
-    var point9 = new Circle();
+    point9 = new Circle();
     point9.radius = 1;
     point9.lineWidth = 5;
     point9.x = clockXpos - 61;
     point9.y = clockYpos;
     point9.color = '#2b0d3b';
     point9.strokeStyle = '#2b0d3b';
-
 
     function animate() {
         stats.begin();
@@ -161,8 +117,6 @@ function init() {
         stats.end();
         requestAnimationFrame(animate);
     }
-
-    animate();
 
     window.setInterval(function () {
         setCurrentTime();
@@ -197,9 +151,65 @@ function init() {
         return bSide;
     }
 
-    // function init() {
-    //     setCurrentTime();
-    //     animate();
-    // }
+    setCurrentTime();
+    animate();
 }
 
+function createCanvas(xpos, ypos, width, height) {
+    var canvas = document.createElement('canvas');
+    document.body.appendChild(canvas);
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.background = "#ee3344";
+    canvas.style.position = 'absolute';
+    canvas.style.left = "" + xpos + "px";
+    canvas.style.top = "" + ypos + "px";
+    return canvas;
+}
+
+function Circle() {
+    this.radius = 100;
+    this.x = 0;
+    this.y = 0;
+    this.lineWidth = 3;
+    this.color = 'none';
+    this.strokeStyle = 'none';
+}
+
+Circle.prototype.draw = function draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    context.fillStyle = this.color;
+    context.fill();
+    context.lineWidth = this.lineWidth;
+    context.strokeStyle = this.strokeStyle;
+
+    context.stroke();
+};
+
+function Arrow() {
+    this.length = 0;
+    this.x = 0;
+    this.y = 0;
+    this.rotation = 0;
+    this.lineWidth = 0;
+    this.color = 'white';
+}
+
+Arrow.prototype.draw = function (context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(0, -this.length);
+    context.lineWidth = this.lineWidth;
+    context.strokeStyle = this.color;
+    context.lineCap = 'round';
+    context.shadowBlur = 6;
+    context.shadowOffsetX = 1;
+    context.shadowOffsetY = 4;
+    context.shadowColor = "rgba(100, 100, 100, 0.75)";
+    context.stroke();
+    context.restore();
+};
